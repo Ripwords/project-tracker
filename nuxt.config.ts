2add435b3defe61c395dbeb51958b1e120cb6ddd
JJ <teohjjteoh@gmail.com>
2025-02-17 01:23:23 +0800

    chore: Simplify PWA manifest configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,34 +30,7 @@ export default defineNuxtConfig({
   },
   css: ["~/assets/css/main.css"],
   pwa: {
-    manifest: {
-      icons: [
-        {
-          src: "public/img/icons/manifest-icon-192.maskable.png",
-          sizes: "192x192",
-          type: "image/png",
-          purpose: "any",
-        },
-        {
-          src: "public/img/icons/manifest-icon-192.maskable.png",
-          sizes: "192x192",
-          type: "image/png",
-          purpose: "maskable",
-        },
-        {
-          src: "public/img/icons/manifest-icon-512.maskable.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "any",
-        },
-        {
-          src: "public/img/icons/manifest-icon-512.maskable.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "maskable",
-        },
-      ],
-    },
+    manifest: {},
   },
   modules: [
     "@nuxt/ui",
